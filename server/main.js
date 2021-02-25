aa34bd8699b7111035c8a90555ae512e5e726a8d
Sean Johnston <seanjohnston@google.com>
2021-02-24 17:14:07 -0800

    update connect-mongo

--- server/main.js
+++ server/main.js
@@ -9,17 +9,13 @@ var startDb = require('./db');
 var server = require('http').createServer();
 
 var createApplication = function () {
-	console.log("Creating application");
     var app = require('./app');
-    console.log("Attaching express application");
     server.on('request', app); // Attach the Express application.
-    console.log("Attaching socketio");
     require('./io')(server);   // Attach socket.io.
 };
 
 var startServer = function () {
 
-    console.log("Starting server");
     var PORT = process.env.PORT || 1337;
 
     server.listen(PORT, function () {
